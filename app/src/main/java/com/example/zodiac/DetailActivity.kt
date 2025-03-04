dc9f5f6d03029774ccb64c563341be56ca890440
Fran876876 <fmartosmuoz@yahoo.es>
2025-03-04 20:09:08 +0100

    menu activity_detail.xml y menu activity_main.xml incorporados

--- app/src/main/java/com/example/zodiac/DetailActivity.kt
+++ app/src/main/java/com/example/zodiac/DetailActivity.kt
@@ -1,6 +1,10 @@
 package com.example.zodiac
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,25 +13,68 @@ import androidx.core.view.WindowInsetsCompat
 
 class DetailActivity : AppCompatActivity() {
 
-    companion object{
-        const val EXTRA_HOROSCOPE_ID = "HOROSCOPE"
+    companion object {
+        const val EXTRA_HOROSCOPE_ID = "HOROSCOPE_ID"
     }
+
+    lateinit var nameTextView: TextView
+    lateinit var dateTextView: TextView
+    lateinit var iconImageView: ImageView
+
+    lateinit var horoscope: Horoscope
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
 
         setContentView(R.layout.activity_detail)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val id = intent.getStringExtra(EXTRA_HOROSCOPE_ID)
+        val id = intent.getStringExtra(EXTRA_HOROSCOPE_ID)!!
+        horoscope = Horoscope.findById(id)
 
+        initView()
 
-        val horoscope = Horoscope.findById(id)
-        findViewById<TextView>(R.id.text).text = "id: ${getString(horoscope.name)}"
+        loadData()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_activity_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_favorite -> {
+                Log.i("MENU", "Menu favorito")
+                true
+            }
+            R.id.action_share -> {
+                Log.i("MENU", "Menu compartir")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun loadData() {
+        supportActionBar?.setTitle(horoscope.name)
+        supportActionBar?.setSubtitle(horoscope.dates)
+
+        nameTextView.setText(horoscope.name)
+        dateTextView.setText(horoscope.dates)
+        iconImageView.setImageResource(horoscope.icon)
+    }
+
+    private fun initView() {
+        nameTextView = findViewById(R.id.nameTextView)
+        dateTextView = findViewById(R.id.dateTextView)
+        iconImageView = findViewById(R.id.iconImageView)
     }
 }
